4c05ea65c59b14539c94d89b9abe5d6340a7092a
William Kang <william@williamk.ga>
2022-05-17 09:43:52 -0400

    arrow functionality

--- index.js
+++ index.js
@@ -7,25 +7,34 @@ const sammies = [
 const prev = document.getElementById('arrow-prev');
 const forw = document.getElementById('arrow-forw');
 
-let currentSammy = 0;
+const currentSammy = {
+    head: 0,
+    body: 0,
+    legs: 0
+};
 
 const changeSammy = () => {
     [ 'head', 'body', 'legs' ].forEach(part => {
         const element = document.getElementById(`sammy-${part}`);
-        element.src = `assets/sammies/${sammies[currentSammy]}/${part}.png`;
+        element.src = `assets/sammies/${sammies[currentSammy][part]}/${part}.png`;
     });
 };
 
-prev.addEventListener('click', () => {
-    currentSammy--;
-    if (currentSammy < 0)
-        currentSammy = sammies.length;
-    changeSammy();
-});
+[ 'head', 'body', 'legs' ].forEach(part => {
+    [ 'prev', 'forw' ].forEach(direction => {
+        const arrow = document.getElementById(`arrow-${direction}-${part}`);
 
-forw.addEventListener('click', () => {
-    currentSammy++;
-    if (currentSammy >= sammies.length)
-        currentSammy = 0;
-    changeSammy();
+        arrow.addEventListener('click', () => {
+            if (direction == 'prev')
+                currentSammy[part]--;
+            else if (direction == 'forw')
+                currentSammy[part]++;
+
+            if (currentSammy < 0)
+             currentSammy = sammies.length;
+            if (currentSammy >= sammies.length)
+             currentSammy = 0;
+            changeSammy();
+        });
+    });
 });
