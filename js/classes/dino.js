5e0eb1f42e5d7ee9943bece0411680eece9049db
William Kang <william@williamk.ga>
2022-06-08 08:57:09 -0400

    more frames anim

--- js/classes/dino.js
+++ js/classes/dino.js
@@ -7,7 +7,7 @@ import {
 const dinoElem = document.querySelector('[data-dino]');
 const JUMP_SPEED = 0.45;
 const GRAVITY = 0.0015;
-const DINO_FRAME_COUNT = 2;
+const DINO_FRAME_COUNT = 4;
 const FRAME_TIME = 100;
 
 let isJumping;
