e2b837508a33e027df6b86721c5eaaa7bd72139f
William Kang <contact@williamk.ga>
2022-06-10 22:14:01 -0400

    fix eruda

--- js/dressmy.js
+++ js/dressmy.js
@@ -108,8 +108,7 @@ colourSchemeButton.onclick = () => {
 }
 
 (function () {
-  var src = 'https://cdn.jsdelivr.net/npm/eruda';
-  if (!/eruda=true/.test(window.location) && localStorage.getItem('active-eruda') != 'true') return;
-  document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
+  if (!/eruda=true/.test(window.location)) return;
+  document.write('<scr' + 'ipt src="https://cdn.jsdelivr.net/npm/eruda"></scr' + 'ipt>');
   document.write('<scr' + 'ipt>eruda.init();</scr' + 'ipt>');
 })();
